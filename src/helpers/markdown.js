b6048940bae772eb84c7a731d674fc0bbe9b850d
carteredge <carter.edge@gmail.com>
2023-11-02 21:24:29 -0400

    prod build

--- src/helpers/markdown.js
+++
@@ -1,6 +0,0 @@
-import * as showdown from "showdown";
-
-export function convertMD(markdown) {
-    const converter = new showdown.Converter();
-    return converter.makeHtml(markdown);
-}
--- src/helpers/text.js
+++
@@ -1,4 +0,0 @@
-export function toLabel(text) {
-    text = text.replace(/([a-z])([A-Z])/g, "$1 $2");
-    return `${text.substring(0,1).toUpperCase()}${text.substring(1)}`;
-}
